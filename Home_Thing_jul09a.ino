6b82f761846ce7d2a7ce43e57ca49fee4201b830
Leo M <leomen3@gmail.com>
2025-02-22 17:28:52 +0200

    Added external reset capability via Arduino cloud

--- Home_Thing_jul09a.ino
+++ Home_Thing_jul09a.ino
@@ -87,46 +87,34 @@ void loop() {
 
 
 void onHARPiSupplySWChange()  {
-  /*
+  
   // Add your code here to act upon HARPiSupplySW change
 
    if (hA_RPi_Supply_SW == 1){
-   //if SW is ON (HIGH), S2 and S4 Relay CONNECTS COM to NO (Normally Open). Otherwise, BOTH COM in connect to NC (Normally Connected)
+   //if SW is ON (HIGH), enable Power Up Sequence (Ground -> Primary power rail -> seconday power rail)
+  Serial.println("Setting S2 to On");
+  digitalWrite(RELAY_S2, HIGH);  // Turn ON S2
+  // wait half a second for ground to stabilize
+  delay(500);
+  Serial.println("Setting S3 to On");
+  digitalWrite(RELAY_S3, HIGH);  // Turn ON S3
+  // wait 5 seconds for boot to start. If primary (S3) and Secondary (S4) turn on at the same time, BOOT DOES NOT START
+  delay(5000);
+  Serial.println("Setting S4 to On");
+  digitalWrite(RELAY_S4, HIGH);  // Turn ON S4
+  
+   }
+   else {
+  Serial.println("Setting S2/3/4 to Off");
+  digitalWrite(RELAY_S4, LOW);
+  digitalWrite(RELAY_S3, LOW);
+  digitalWrite(RELAY_S2, LOW);
+  
+
+
+   }
+   delay(500);
    
-   digitalWrite(16,HIGH); //IMPORTANT, FIRST DISCONNECT S2, SINCE IT IS CONNECTED TO GROUND
-   delay(500);
-   digitalWrite(12,HIGH);
-   }
-   else {
-   digitalWrite(12,LOW); //IMPORTANT, FIRST DISCONNECT S4, SINCE IT IS CONNECTED TO 5V
-   delay(500);
-   digitalWrite(16,LOW);
-   delay(500);
-
-   }
-   delay(500);
-   */
-}
-
-
-
-
-/*
-  Since S3Relay is READ_WRITE variable, onS3RelayChange() is
-  executed every time a new value is received from IoT Cloud.
-*/
-void onS3RelayChange()  {
-  /*// Add your code here to act upon S3Relay change
-   if (s3_Relay == 1){
-   //if SW is ON (HIGH), S3 Relay CONNECTS COM to NO (Normally Open). Otherwise, COM in connect to NC (Normally Connected)
-   digitalWrite(14,HIGH);
-   Serial.println("Relay to NO\n");
-   }
-   else {
-   digitalWrite(14,LOW);
-   Serial.println("Relay to NC\n");
-   }
-   delay(1000);*/
 }
 
 
